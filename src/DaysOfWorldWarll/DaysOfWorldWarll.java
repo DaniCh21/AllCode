9683291b5ca9bee9a6c5665a1b383afa95211a18
DaniCh <cd122011@gmail.com>
2018-08-19 17:36:50 +0300

    finished :))

--- src/DaysOfWorldWarll/DaysOfWorldWarll.java
+++ src/DaysOfWorldWarll/DaysOfWorldWarll.java
@@ -4,20 +4,20 @@ import java.util.*;
 
 public class DaysOfWorldWarll {
     public static void main(String[] args){
-        GregorianCalendar startOfWar = new GregorianCalendar(1939, 9, 1);
-        GregorianCalendar endOfWar = new GregorianCalendar(1945, 9, 2);
-        allWar(startOfWar);
-        int dayOfStart = startOfWar.get(Calendar.DAY_OF_YEAR);
-        int dayOfEnd = endOfWar.get(Calendar.DAY_OF_YEAR);
+        GregorianCalendar start = new GregorianCalendar(1939, 9, 1);
+        GregorianCalendar end = new GregorianCalendar(1945, 9, 2);
+        allWar(start, end);
+        int dayOfStart = start.get(Calendar.DAY_OF_YEAR);
+        int dayOfEnd = end.get(Calendar.DAY_OF_YEAR);
         System.out.println("War was " + (daysOfWar - dayOfStart + dayOfEnd) + " days long.");
     }
 
-    public static int daysOfWar = 0;
+    private static int daysOfWar = 0;
 
-    private static void allWar(GregorianCalendar g){
-        GregorianCalendar g1 = new GregorianCalendar(2000, 4 , 5);
-        for (int i = 0; i < 6; i++){
-            g1.set(g.get(Calendar.YEAR)+ i, Calendar.DECEMBER, 31);
+    private static void allWar(GregorianCalendar start, GregorianCalendar end){
+        GregorianCalendar g1 = new GregorianCalendar();
+        for (int i = 0; i < end.get(Calendar.YEAR) - start.get(Calendar.YEAR); i++){
+            g1.set(start.get(Calendar.YEAR)+ i, Calendar.DECEMBER, 31);
             daysOfWar += g1.get(Calendar.DAY_OF_YEAR);
         }
     }
