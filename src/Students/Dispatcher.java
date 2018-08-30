ef14c85a6645f0f2a0d40fcfcc0a8da1086f276c
DaniCh <cd122011@gmail.com>
2018-08-30 20:58:12 +0300

    Dispatcher added

--- src/Students/Dispatcher.java
+++ src/Students/Dispatcher.java
@@ -2,7 +2,51 @@ package Students;
 
 class Dispatcher{
     public static void main(String[] args) {
-        Student Ann = new Student("Ann", 11.1111);
-        Student Den = new Student("Den", 9.11);
+        Group da = new Group(null);
+        Student Petrow = new Student("Vasilii", 9.6);
+        Student Mogutov = new Student("Ivan", 10.8);
+        Student Gasanov = new Student("Jessie", 6.5);
+        Student Petrenko = new Student("Iwar", 11.6);
+        Student Ivanov = new Student("Vasilii", 8.9);
+
+        da.addStudent(Petrenko);
+        da.addStudent(Petrow);
+        da.addStudent(Mogutov);
+        da.addStudent(Gasanov);
+        da.addStudent(Ivanov);
+
+        da.addStudent(new Student("Karlos", 8.0));
+        da.addStudent(new Student("Karlos2", 8.0));
+        da.kickStudent(3);
+        da.addStudent(new Student("Major", 11.99));
+        da.addStudent(new Student("Karina", 10.4));
+        da.addStudent(new Student("Karina2", 10.4));
+        da.addStudent(new Student("Karina3", 10.4));
+        da.addStudent(new Student("Karina4", 10.4));
+
+
+        da.kickStudent(7);
+        da.addStudent(new Student("Karliiitoos", 8.0));
+        System.out.println("///////////////////////////////////////");
+        da.displayAllCurrentStudents();
+        da.kickStudent(2);
+        da.displayAllCurrentStudents();
+        da.addStudent(new Student("Lololoshka", 10.1));
+        da.displayAllCurrentStudents();
+        da.displayKickedStudents();
+        da.changeStudentsMarks(6, 9.9);
+        da.displayAllCurrentStudents();
+
+        System.out.println("finalized\n");
+        da = new Group(da);
+        da.displayAllCurrentStudents();
+        da.displayKickedStudents();
+
+        System.out.println("edited\n");
+
+        System.out.println("status: " + da.addStudent(new Student("Karina5", 10.4)));
+
+        da.displayAllCurrentStudents();
+
     }
 }
--- src/Students/Group.java
+++ src/Students/Group.java
@@ -9,7 +9,7 @@ class Group {
     int numberOfKicked = 0;
     final Student namesOfKicked[] = new Student[MAX_STUDENTS];
 
-    private boolean changeStudentsMarks(int index, double newMark) {
+    boolean changeStudentsMarks(int index, double newMark) {
         if (!isChangeable) {
             System.out.println("Can't change, because this is final group");
             return false;
@@ -25,7 +25,7 @@ class Group {
         return true;
     }
 
-    private boolean addStudent(Student student) {
+    boolean addStudent(Student student) {
 
         if (!isChangeable) {
             System.out.println("Can't change, because this is final group");
@@ -40,11 +40,10 @@ class Group {
             System.out.println("This group already have 30 students.");
             return false;
         }
-
         return true;
     }
 
-    private boolean kickStudent(int index) {
+    boolean kickStudent(int index) {
         if (!isChangeable) {
             System.out.println("Can't change, because this is final group");
             return false;
@@ -70,7 +69,7 @@ class Group {
         return true;
     }
 
-    protected void displayAllCurrentStudents() {
+    void displayAllCurrentStudents() {
         System.out.println("////////////////////////////////////");
         System.out.println("CURRENT STUDENTS IN GROUP:");
 
@@ -79,10 +78,9 @@ class Group {
         }
 
         System.out.println("////////////////////////////////////");
-
     }
 
-    protected void displayKickedStudents() {
+    void displayKickedStudents() {
         System.out.println("------------------------------------");
         System.out.println("These students were kicked:");
 
@@ -91,7 +89,6 @@ class Group {
         }
 
         System.out.println("------------------------------------");
-
     }
 
 
@@ -112,61 +109,7 @@ class Group {
             this.numberOfStudents =
                     group.numberOfStudents;
             this.numberOfKicked = group.numberOfKicked;
-
         }
-
-    }
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    public static void main(String[] args) {
-        Group da = new Group(null);
-        Student Petrow = new Student("Vasilii", 9.6);
-        Student Mogutov = new Student("Ivan", 10.8);
-        Student Gasanov = new Student("Jessie", 6.5);
-        Student Petrenko = new Student("Iwar", 11.6);
-        Student Ivanov = new Student("Vasilii", 8.9);
-
-        da.addStudent(Petrenko);
-        da.addStudent(Petrow);
-        da.addStudent(Mogutov);
-        da.addStudent(Gasanov);
-        da.addStudent(Ivanov);
-
-        da.addStudent(new Student("Karlos", 8.0));
-        da.addStudent(new Student("Karlos2", 8.0));
-        da.kickStudent(3);
-        da.addStudent(new Student("Major", 11.99));
-        da.addStudent(new Student("Karina", 10.4));
-        da.addStudent(new Student("Karina2", 10.4));
-        da.addStudent(new Student("Karina3", 10.4));
-        da.addStudent(new Student("Karina4", 10.4));
-
-
-        da.kickStudent(7);
-        da.addStudent(new Student("Karliiitoos", 8.0));
-        System.out.println("///////////////////////////////////////");
-        da.displayAllCurrentStudents();
-        da.kickStudent(2);
-        da.displayAllCurrentStudents();
-        da.addStudent(new Student("Lololoshka", 10.1));
-        da.displayAllCurrentStudents();
-        da.displayKickedStudents();
-        da.changeStudentsMarks(6, 9.9);
-        da.displayAllCurrentStudents();
-        da.displayAllCurrentStudents();
-
-        System.out.println("finalized\n");
-        da = new Group(da);
-        da.displayAllCurrentStudents();
-        da.displayKickedStudents();
-
-        System.out.println("edited\n");
-
-        System.out.println("status: " + da.addStudent(new Student("Karina5", 10.4)));
-
-        da.displayAllCurrentStudents();
-
-
     }
 }
 
